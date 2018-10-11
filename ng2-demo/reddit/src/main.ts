ac731cc4d94b3f1577140a93c6644517a7f58491
ych <895096980@qq.com>
2018-10-11 10:05:46 +0800

    Update main.ts

--- ng2-demo/reddit/src/main.ts
+++ ng2-demo/reddit/src/main.ts
@@ -1,10 +1,11 @@
+// main.ts启动应用的地方
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
-if (environment.production) {
-  enableProdMode();
-}
+if(environment.production) enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
