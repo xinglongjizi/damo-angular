b2fc943e556a330c3a79f6e419f1ef31ab997185
ych <895096980@qq.com>
2018-10-11 10:12:42 +0800

    Update app.component.ts

--- ng2-demo/reddit/src/app/app.component.ts
+++ ng2-demo/reddit/src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+// 应用的根组件，每个angular应用都至少要有一个根组件(root component)
+
+import { Component } from '@angular/core';			// 因为定义的是根组件，因此要导入此Component装饰器，好使用@Component装饰器来修饰组件
 import { Article } from './article/article.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',					// 通过<app-root></app-root>使用
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-
+// export指定了组件可以再文件外使用，即是可被导出的
 export class AppComponent {
 	articles: Array<Article>;
 
