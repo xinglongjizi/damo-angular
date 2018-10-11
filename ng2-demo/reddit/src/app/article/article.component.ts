9b6d2445fb8299553e794ad99d4dd9df2a9acbff
ych <895096980@qq.com>
2018-10-11 11:06:51 +0800

    Update article.component.ts

--- ng2-demo/reddit/src/app/article/article.component.ts
+++ ng2-demo/reddit/src/app/article/article.component.ts
@@ -10,6 +10,14 @@ import { Article } from './article.model';
   },
 })
 
+/*
+通过<app-article></app-article>来使用此组件
+<app-article></app-article>标签的实际的html标签是article.component.html中的html内容
+组件的输入输出属性写法都是写在使用<app-article></app-article>的地方，即就写在<app-article></app-article>上:
+比如<app-article [childParam]="parentParam" (action)="something"></app-article>
+
+而通常的DOM事件绑定(比如click等)，还是写在实际的html标签上，即article.component.html中的html内容中
+*/
 
 export class ArticleComponent implements OnInit {
 	// votes: number;
